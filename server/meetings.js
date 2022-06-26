256397c78544f17b476090b801f2b13caac5f864
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-06-26 17:47:20 +0100

    meetings finished

--- server/meetings.js
+++ server/meetings.js
@@ -1,4 +1,20 @@
-const meetings = () => {};
+const meetings = require('express').Router();
+
+const { getAllFromDatabase, addToDatabase, deleteAllFromDatabase, createMeeting } = require('./db');
+
+meetings.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('meetings'));
+});
+
+meetings.post('/', (req, res, next) => {
+  let newMeeting = addToDatabase('meetings', createMeeting());
+  res.status(201).send(newMeeting);
+});
+
+meetings.delete('/', (req, res, next) => {
+  deleteAllFromDatabase('meetings');
+  res.status(204).send();
+});
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = meetings;
